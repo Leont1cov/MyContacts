80869b9ec54b90894726de4f032e34037bb12324
Leont1cov <chuchunovvakov@mail.ru>
2025-12-30 12:54:58 +0300

    feat(card): add markup Card.tsx

---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+const Card = () => {
+    return (
+        <>
+            <div>
+                <img src="#" alt=""/>
+                <a href="#">
+                    lorem ipsum
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default Card;
