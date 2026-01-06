46ae79e3e4bbcd2df4b1d19e9ab9d3135443bb1d
Leont1cov <chuchunovvakov@mail.ru>
2026-01-06 22:26:30 +0300

    feat: create Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Card from "./components/Card/Card.tsx";
+import {faFacebook, faGithub, faLinkedin, faTelegram, faVk, faYoutube} from "@fortawesome/free-brands-svg-icons";
+
 function App() {
   return (
     <>
-      <h1>Hello world</h1>
+        <div className="container p-10 flex flex-col gap-3">
+            <Card text={"Это мой телеграмм, пишите, звоните"} icon={faTelegram}/>
+            <Card text={"Тут мой ВК подписывайтесь"} icon={faVk}/>
+            <Card text={"Мой репозитоий"} icon={faGithub}/>
+            <Card text={"Мой ютуб канал"} icon={faYoutube}/>
+            <Card text={"Мой Facebook"} icon={faFacebook}/>
+            <Card text={"Мой LinkedIn"} icon={faLinkedin}/>
+
+        </div>
     </>
   )
 }
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,35 @@
+import "./Card.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faTelegram} from "@fortawesome/free-brands-svg-icons";
+import {faVk} from "@fortawesome/free-brands-svg-icons";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import {faYoutube} from "@fortawesome/free-brands-svg-icons";
+import {faTwitter} from "@fortawesome/free-brands-svg-icons";
+import {faFacebook} from "@fortawesome/free-brands-svg-icons";
+import {faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import {faTiktok} from "@fortawesome/free-brands-svg-icons";
+
+type CardProps = {
+    text: string;
+    icon?: typeof faTelegram | typeof faVk | typeof faGithub | typeof faYoutube | typeof faTwitter | typeof faFacebook | typeof faLinkedin | typeof faTiktok;
+}
+
+const Card = ({ text, icon }:CardProps) => {
+    if (!icon) return null;
+
+    return (
+        <>
+            {/*Тут будет ссылка на нужную соц.сеть*/}
+            <a href="#" className="flex justify-center items-center gap-3 max-w-90
+            text-xl text-center p-5 border border-solid border-gray-300 rounded-lg hover:bg-gray-200 transition duration-200 ease-in-out">
+                <div className="text-center">
+                    <FontAwesomeIcon icon={icon} className="text-blue-600 text-2xl" />
+                </div>
+
+                {text ?? "Здесь будет ваш текст!"}
+            </a>
+        </>
+    )
+}
+
+export default Card;
