1267ac4ed9bd3907899264134da9666cd21c4fce
Leont1cov <chuchunovvakov@mail.ru>
2026-01-08 22:56:46 +0300

    refactor(Button): add new type for component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,18 @@
 import "../../styles/App.css";
+import * as React from "react";
 
 type ButtonProps = {
-    children?: string;
+    children?: string | React.ReactNode;
     onClick?: () => void;
     type?: "button" | "submit";
+    className?: string;
 }
 
-const Button = ({ children, onClick, type }:ButtonProps) => {
+const Button = ({ children, onClick, type, className }:ButtonProps) => {
     return (
         <>
-            <button className="border border-solid border-gray-300 rounded-lg max-w-90 p-2 hover:bg-gray-200
-            transition duration-200 ease-in-out active:bg-gray-300 cursor-pointer" onClick={onClick} type={type}>
+            <button className={`${className} border border-solid border-gray-300 rounded-lg max-w-90 p-2 hover:bg-gray-200
+            transition duration-200 ease-in-out active:bg-gray-300 cursor-pointer`} onClick={onClick} type={type}>
                 {children}
             </button>
         </>
