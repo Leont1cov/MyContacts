4c4b98e52131a4b6204e81597008c8292d97f33c
Leont1cov <chuchunovvakov@mail.ru>
2026-01-13 17:42:52 +0300

    style(Hero): add ShinyText

--- src/pages/Landing/Hero/Hero.tsx
+++ src/pages/Landing/Hero/Hero.tsx
@@ -1,14 +1,23 @@
 import Button from "../../../components/Button/Button.tsx";
-
+import ShinyText from './ReactBits/ShinyText';
 
 const Hero = () => {
     return (
         <>
             <div className="flex flex-col px-5 sm:px-10 lg:px-30 gap-8 bg-white">
                 {/* Заголовок */}
-                <h1 className="text-2xl sm:text-7xl lg:text-8xl font-medium leading-tight">
-                    MyContacts
-                </h1>
+                <ShinyText
+                    text="MyContacts"
+                    speed={2}
+                    delay={0}
+                    color="#2A2A2A"
+                    shineColor="#FFD8A8"
+                    spread={120}
+                    direction="left"
+                    yoyo={false}
+                    pauseOnHover={false}
+                    className={"text-2xl sm:text-7xl lg:text-8xl font-medium leading-tight"}
+                />
 
                 {/* Подзаголовок */}
                 <p className="text-lg sm:text-xl lg:text-2xl text-gray-600 max-w-xl leading-relaxed">
