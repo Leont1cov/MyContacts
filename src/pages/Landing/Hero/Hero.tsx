cfe04341333a88e2b6aa6bf193b3cd5467d29f1d
Leont1cov <chuchunovvakov@mail.ru>
2026-01-14 15:51:10 +0300

    style(Hero): add animate for content

--- src/pages/Landing/Hero/Hero.tsx
+++ src/pages/Landing/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import ShinyText from './ReactBits/ShinyText';
 const Hero = () => {
     return (
         <>
-            <div className="flex flex-col justify-center items-center text-center mt-15 px-5 sm:px-10 lg:px-30 gap-8 bg-white">
+            <div className="flex flex-col justify-center items-center text-center mt-20 px-5 sm:px-10 lg:px-30 gap-4 bg-white">
                 {/* Заголовок */}
                 <ShinyText
                     text="MyContacts"
@@ -16,15 +16,19 @@ const Hero = () => {
                     direction="left"
                     yoyo={false}
                     pauseOnHover={false}
-                    className={"text-2xl sm:text-7xl lg:text-8xl font-medium leading-tight"}
+                    className={"text-2xl sm:text-7xl lg:text-8xl font-medium leading-tight animate-[fadeUp_0.6s_ease-out_forwards]"}
                 />
 
                 {/* Подзаголовок */}
-                <p className="text-lg sm:text-xl lg:text-2xl text-gray-600 max-w-xl leading-relaxed">
+                <p className="
+                    text-lg sm:text-xl lg:text-2xl text-gray-600 max-w-xl leading-relaxed
+                    opacity-0 translate-y-4
+                    animate-[fadeUp_0.6s_ease-out_forwards]
+                  ">
                     Все ваши контакты в одном удобном месте. Управляйте, находите и сохраняйте — без хаоса.
                 </p>
 
-                <Button>
+                <Button className="mt-10 animate-[fadeUp_0.6s_ease-out_forwards]">
                     Попробовать
                 </Button>
             </div>
