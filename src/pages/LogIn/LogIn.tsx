dcd9063e0cb4df5ff93ed22ce9f19db71a7d23a0
Leont1cov <chuchunovvakov@mail.ru>
2026-01-15 18:04:45 +0300

    feat: initial LogIn component

---
+++ src/pages/LogIn/LogIn.tsx
@@ -0,0 +1,9 @@
+const Login = () => {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1>Hello world</h1>
+        </div>
+    );
+};
+
+export default Login;
