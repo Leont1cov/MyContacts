b9d30c8b0de93e9b73d371864b4223ac455c4f20
Leont1cov <chuchunovvakov@mail.ru>
2026-01-16 14:04:26 +0300

    feat(Button): add Font-awesome props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,18 +1,27 @@
 import "../../styles/App.css";
 import * as React from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import type {IconDefinition} from "@fortawesome/fontawesome-svg-core";
 
 type ButtonProps = {
     children?: string | React.ReactNode;
     onClick?: () => void;
     type?: "button" | "submit";
     className?: string;
+    icon: IconDefinition;
+    iconColor?: string;
 }
 
-const Button = ({ children, onClick, type, className }:ButtonProps) => {
+const Button = ({ children, onClick, type, className, icon, iconColor }:ButtonProps) => {
     return (
         <>
             <button className={`${className} border border-solid border-gray-300 rounded-lg max-w-90 p-2 hover:bg-gray-200
             transition duration-200 ease-in-out active:bg-gray-300 cursor-pointer`} onClick={onClick} type={type}>
+                <FontAwesomeIcon
+                    icon={icon}
+                    className="text-2xl"
+                    style={{ color: iconColor }}
+                />
                 {children}
             </button>
         </>
