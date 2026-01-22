30b0ae30becc5598c8bc94be8752a604981754e8
Leont1cov <chuchunovvakov@mail.ru>
2026-01-22 17:02:40 +0300

    style(About): add animation useInView

--- src/pages/Landing/About/About.tsx
+++ src/pages/Landing/About/About.tsx
@@ -1,6 +1,21 @@
+import { useInView } from 'react-intersection-observer';
+
 const About = () => {
+    const { ref, inView } = useInView({
+        triggerOnce: false, // сработает один раз
+        threshold: 0.3,    // появляется, когда 30% блока видно
+    });
+
     return (
-        <section className={"flex flex-col lg:flex-row items-center justify-center mt-32 lg:mt-40 px-5 sm:px-10 lg:px-30 gap-10 bg-white"}>
+        <section
+            ref={ref}
+            className={`
+            flex flex-col lg:flex-row items-center justify-center mt-32 lg:mt-40
+            px-5 sm:px-10 lg:px-30 gap-10 bg-white
+            transition-all duration-700 ease-out
+            ${inView ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"}
+          `}
+        >
 
             <div className="relative w-full max-w-md mx-auto">
                 <div className="
