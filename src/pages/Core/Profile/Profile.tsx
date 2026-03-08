6fae48de3853bfc4571e063fbd6b1ababd320c35
Leont1cov <chuchunovvakov@mail.ru>
2026-03-08 21:54:29 +0300

    feat: merge from feature/sessions to main

--- src/pages/Core/Profile/Profile.tsx
+++ src/pages/Core/Profile/Profile.tsx
@@ -16,7 +16,7 @@ const Profile = () => {
     const [isOpen, setIsOpen] = useState(false);
     const [editIndex, setEditIndex] = useState<number | null>(null);
 
-    // 2️⃣ Функция загрузки ссылок из Supabase
+    // Функция загрузки ссылок из Supabase
     async function fetchLinks(userId: string) {
         setLoading(true);
         const { data, error } = await supabase
